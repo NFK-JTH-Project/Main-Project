dc0312527d212d4412ca94f23b8cbdf0fa3ff899
dennis <molund1994@gmail.com>
2020-05-03 12:36:50 +0200

    Added Navigation activity. When clicked on a searchItem the user now gets sent to the Navigation Activity

--- app/src/main/java/com/example/nfk_project/MainActivity.kt
+++ app/src/main/java/com/example/nfk_project/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.nfk_project
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
+import android.view.View
+import android.widget.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -17,10 +17,6 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var editTextSearch:EditText
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,14 +39,26 @@ class MainActivity : AppCompatActivity() {
 
         val search_bar = findViewById<AutoCompleteTextView>(R.id.search_bar)
 
-        //Todo: Get strings from resources, Errors: cannot have spaces between words.
+        //todo Errors: cannot have spaces between words.
         val suggestions = resources.getStringArray(R.array.suggestions)
+
+        //Tells the autocomplete to work from the users first letter, default is 2 letters.
+        search_bar.threshold = 0
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, suggestions)
             search_bar.setAdapter(adapter)
 
-
-
+        //On clicklistener that triggers when a suggestion is clicked, should take user to navigation activity
+        search_bar.onItemClickListener = AdapterView.OnItemClickListener{
+                parent,view,position,id->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            // Display the clicked item using toast
+            Toast.makeText(applicationContext,"Selected : $selectedItem",Toast.LENGTH_SHORT).show()
+            //todo take user to correct navigation activity, room/person is stored in selectedItem
+            val intent = Intent(this, NavigationActivity::class.java)
+            intent.putExtra("value", selectedItem)
+            startActivity(intent)
+        }
 
 
     }
