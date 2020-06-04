fdebdee7419a53004f3d960b8200a9865651a158
Rasmus Svanberg <rasmus.svahnberg97@gmail.com>
2020-06-04 14:49:05 +0200

    Added highlights images for all the campus buildings

--- app/src/main/java/com/example/nfk_project/MapCreator/BitmapRepository.kt
+++ app/src/main/java/com/example/nfk_project/MapCreator/BitmapRepository.kt
@@ -203,6 +203,10 @@ class BitmapRepository {
 
         /* *** *** *** *** *** CAMPUS *** *** *** *** *** */
 
+        bitmap = BitmapFactory.decodeResource(res, R.drawable.floor4_layer_top)
+        layer = ImageLayer(bitmap, true, MapKeyword.f4Top)
+        floor4.add(layer)
+
         finished(true)
     }
 
